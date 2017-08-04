5d8f3b7a14ff3bd6ee5d736e1b5de992b4938e98
Ernesto Coto <ernesto.coto@gmail.com>
2017-08-04 16:56:29 +0100

    feat: Making the default home redirect to the site-prefix home

--- visorgen/urls.py
+++ visorgen/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import RedirectView
 
 SITE_PREFIX = settings.SITE_PREFIX.replace('/','') # make sure to avoid any extra /
 
 urlpatterns = [
     url(r'^%s/admin/' % SITE_PREFIX, admin.site.urls),
     url(r'^%s/' % SITE_PREFIX , include('siteroot.urls')),
+    url(r'^$', RedirectView.as_view(url='%s/' % SITE_PREFIX, permanent=False)),
 ]
 
 # override 404 handler to show customised page
