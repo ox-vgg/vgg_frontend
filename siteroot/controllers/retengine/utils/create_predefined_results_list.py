105a12e3b1f69aa47ec055b1f751cbe4b59493c8
Ernesto Coto <ernesto.coto@gmail.com>
2018-03-20 14:24:28 +0000

    maint: Adding template to create pre-defined lists

---
+++ siteroot/controllers/retengine/utils/create_predefined_results_list.py
@@ -0,0 +1,13 @@
+#  The line below requires msgpack-python==0.3.0 (if not already installed).
+#  Install it by running: 'pip install msgpack-python==0.3.0'
+import msgpack
+
+query_text = 'name-of-predefined-list-of-results'
+output_file_name = 'mydataset___text__{%s}.msgpack' % query_text
+results_list = []
+# This is the pattern: results_list.append( {'path': 'path-relative-to-image-data-folder' ,'roi': 'x1_y1_x2_y1_x2_y2_x1_y2_x1_y1' } )
+# (x1,y1) corresponds to the top-left corner of the ROI and (x2,y2) to the bottom-right corner.
+# This is an example:
+results_list.append( {'path': 'master_list/box21/dvd306_062.tif.jpg', 'roi': '195.01_183.58_297.24_183.58_297.24_285.81_195.01_285.81_195.01_183.58'} )
+with open(output_file_name, 'wb') as rfile:
+    msgpack.dump(results_list, rfile)
