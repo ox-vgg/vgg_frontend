d40a636c24e1511e15d9c80d9f6471b2fa09d9f0
Ernesto Coto <ernesto.coto@gmail.com>
2020-03-26 14:12:24 +0000

    enh: Updating the vgg_front_end for display installer to the new python3 version

--- install/ubuntu/install_vgg_frontend_ubuntu.sh
+++ install/ubuntu/install_vgg_frontend_ubuntu.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 # - This script is to be run in a clean Ubuntu 16 LTS machine, by a sudoer user.
-# - The directory /webapps/ should not exist
+# - The directory /webapps/ should not exist.
+# - A python3 virtualenv is created in /webapps/visorgen/ wich needs to be activated to run the application.
 
 # update repositories
 sudo apt-get update
 
-# pip and python dependencies
-sudo apt-get install -y python-pip
-sudo apt-get install -y python-dev
+# install all apt-get dependencies
+sudo apt-get install -y python-pip python3-pip
+sudo apt-get install -y python-dev python3-dev
+sudo apt-get install -y memcached
+sudo apt-get install -y libz-dev libjpeg-dev libfreetype6-dev
+sudo apt-get install -y libzmq-dev
+
+# install virtualenv
+sudo pip install configparser==4.0.2
+sudo pip install virtualenv==20.0.7
+pip install --upgrade pip
+pip install zipp
 
 # create main folders
 sudo mkdir /webapps/
 sudo chmod 777 /webapps/
 mkdir /webapps/visorgen
+cd /webapps/visorgen
+virtualenv -p python3 .
+source ./bin/activate
 
 # create data folders
 mkdir /webapps/visorgen/datasets
@@ -24,22 +37,20 @@ mkdir /webapps/visorgen/frontend_data/searchdata/predefined_rankinglists
 mkdir /webapps/visorgen/frontend_data/searchdata/predefined_rankinglists/display
 
 # Django dependencies
-sudo pip install django==1.10
-sudo apt-get install -y memcached
-sudo pip install python-memcached
+pip install django==1.10
+pip install python-memcached
 
 # frontend dependencies
-sudo apt-get install -y libz-dev libjpeg-dev libfreetype6-dev
-sudo pip install protobuf==3.0.0
-sudo pip install Pillow==2.3.0
-sudo pip install Whoosh==2.7.4
+pip install protobuf==3.0.0
+pip install Pillow==2.3.0
+pip install Whoosh==2.7.4
+pip install simplejson==3.8.2
 
 # controller dependencies
-sudo apt-get install -y libzmq-dev
-sudo pip install validictory==0.9.1
-sudo pip install msgpack-python==0.3.0
-sudo pip install requests==2.2.1
-sudo pip install pyzmq==17.1.2
+pip install validictory==0.9.1
+pip install msgpack-python==0.3.0
+pip install requests==2.2.1
+pip install pyzmq==17.1.2
 
 # download and setup vgg_frontend for JUST displaying results
 wget https://gitlab.com/vgg/vgg_frontend/-/archive/master/vgg_frontend-master.zip -O /tmp/vgg_frontend.zip
@@ -50,4 +61,3 @@ echo '%45yak9wu56^(@un!b+&022fdr!-1@92_u*gctw*cw4*@hfu5t' > /webapps/visorgen/se
 cp /webapps/visorgen/vgg_frontend/visorgen/settings_display.py /webapps/visorgen/vgg_frontend/visorgen/settings.py
 cd /webapps/visorgen/vgg_frontend
 python manage.py migrate
-
