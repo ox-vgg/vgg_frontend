136445564c6f73a965e44525d47d6125000e313d
Ernesto Coto <ernesto.coto@gmail.com>
2020-03-27 16:37:05 +0000

    enh: Updating the VIC ubuntu installer to the new frontend version

--- install/ubuntu/install_vic_ubuntu.sh
+++ install/ubuntu/install_vic_ubuntu.sh
@@ -2,20 +2,16 @@
 
 # - This script is to be run in a clean Ubuntu 16 LTS machine, by a sudoer user.
 # - The directory /webapps/ should not exist
+# - Caffe is compiled for CPU if nvidia-smi cannot be found in the system.
 # - It creates one user for the Admin Tools: login/passwd --> admin/vggadmin
-# - It assumes the CUDA Toolkit and drivers are installed in your PC. Make sure you can compile and run the CUDA Toolkit Samples.
-# - CUDA must be accesible, define the environment variables below to suit your local setup. The same variables should be
-#   available when actually running VIC.
-
-#export PATH=/usr/local/cuda/bin:$PATH
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/lib
+# - Start the application with /webapps/visorgen/vgg_frontend/scripts/ start_all_django.sh category
 
 # update repositories
 sudo apt-get update
 
 # pip and python dependencies
-sudo apt-get install -y python-pip
-sudo apt-get install -y python-dev
+sudo apt-get install -y python-pip python3-pip
+sudo apt-get install -y python-dev python3-dev
 
 # install Caffe dependencies
 sudo apt-get install -y cmake
@@ -32,57 +28,33 @@ sudo apt-get install -y libsnappy-dev
 sudo apt-get install -y libgflags-dev
 sudo apt-get install -y --no-install-recommends libboost-all-dev
 
-# dependencies for start/stop scripts
-sudo apt-get install -y screen
-
-# controller dependencies
-sudo apt-get install -y libzmq-dev
-# liblinear installed below is also a dependency.
-# caffe installed below is also a dependency.
-# cpp-netlib installed below is also a dependency.
-
 # cpp-netlib dependencies
 sudo apt-get install -y libssl-dev
 
 # frontend dependencies
 sudo apt-get install -y libz-dev libjpeg-dev libfreetype6-dev
+sudo apt-get install -y screen
+sudo apt-get install -y libzmq-dev
 
 # vgg_classifier dependencies
 sudo apt-get install -y libevent-dev
-sudo pip install numpy==1.11.1
-sudo pip install Pillow==2.3.0
-sudo pip install matplotlib==1.5.3
 
 # Django dependencies
 sudo apt-get install -y memcached
 
-# update setuptools
-sudo pip install setuptools==39.1.0
+# install virtualenv
+sudo pip install configparser==4.0.2
+sudo pip install virtualenv==20.0.7
+pip install --upgrade pip
+pip install zipp
 
-# Django python dependencies
-sudo pip install django==1.10
-sudo pip install python-memcached
-
-# frontend python dependencies
-sudo pip install protobuf==3.0.0
-sudo pip install Whoosh==2.7.4
-
-# vgg_img_downloader python dependencies
-sudo pip install greenlet==0.4.15
-sudo pip install gevent==0.13.8
-sudo pip install Flask==0.10.1
-sudo pip install pyopenssl==17.5.0 pyasn1 ndg-httpsclient
-
-# controller python dependencies
-sudo pip install validictory==0.9.1
-sudo pip install msgpack-python==0.3.0
-sudo pip install requests==2.2.1
-sudo pip install pyzmq==17.1.2
-
-# create main folders
+# create main folder and virtualenv
 sudo mkdir /webapps/
 sudo chmod 777 /webapps/
 mkdir /webapps/visorgen
+cd /webapps/visorgen
+virtualenv -p python3 .
+source ./bin/activate
 
 # create app folders
 mkdir /webapps/visorgen/backend_data /webapps/visorgen/backend_data/cpuvisor-srv
@@ -98,9 +70,31 @@ mkdir /webapps/visorgen/frontend_data/searchdata/rankinglists
 mkdir /webapps/visorgen/frontend_data/searchdata/uploadedimgs
 mkdir /webapps/visorgen/backend_dependencies
 
-# For the changes below, see https://github.com/mistio/mist-ce/issues/434#issuecomment-86484952
-sudo sed -i 's|PROTOCOL_SSLv3|PROTOCOL_SSLv23|g' /usr/local/lib/python2.7/dist-packages/gevent/ssl.py
-sudo sed -i 's|ssl.PROTOCOL_SSLv3|#ssl.PROTOCOL_SSLv3|g' /usr/local/lib/python2.7/dist-packages/requests/packages/urllib3/contrib/pyopenssl.py
+# vgg_classifier dependencies
+pip install numpy==1.11.1
+pip install Pillow==6.1.0
+pip install matplotlib==2.1.0
+
+# Django python dependencies
+pip install django==1.10
+pip install python-memcached
+
+# frontend python dependencies
+pip install protobuf==3.0.0
+pip install Whoosh==2.7.4
+pip install simplejson==3.8.2
+
+# vgg_img_downloader python dependencies
+pip install greenlet==0.4.15
+pip install gevent==1.1.0
+pip install Flask==0.10.1
+pip install pyopenssl==17.5.0 pyasn1 ndg-httpsclient
+
+# controller python dependencies
+pip install validictory==0.9.1
+pip install msgpack-python==0.3.0
+pip install requests==2.2.1
+pip install pyzmq==17.1.2
 
 # download caffe
 sudo apt-get install -y unzip
@@ -122,7 +116,7 @@ unzip /tmp/vgg_classifier.zip -d /webapps/visorgen/
 mv /webapps/visorgen/vgg_classifier*  /webapps/visorgen/vgg_classifier
 
 # download vgg_frontend repo
-wget https://gitlab.com/vgg/vgg_frontend/-/archive/master/vgg_frontend-master.zip -O /tmp/vgg_frontend.zip
+wget https://gitlab.com/vgg/vgg_frontend/-/archive/develop/vgg_frontend-develop.zip -O /tmp/vgg_frontend.zip
 unzip /tmp/vgg_frontend.zip -d /webapps/visorgen/
 mv /webapps/visorgen/vgg_frontend*  /webapps/visorgen/vgg_frontend
 
@@ -130,9 +124,19 @@ mv /webapps/visorgen/vgg_frontend*  /webapps/visorgen/vgg_frontend
 rm -rf /tmp/*.zip
 
 # compile caffe
+# N.B.: No need to setup pycaffe
 cd /webapps/visorgen/backend_dependencies/caffe/
 cp Makefile.config.example Makefile.config
-sed -i 's/\/usr\/include\/python2.7/\/usr\/include\/python2.7 \/usr\/local\/lib\/python2.7\/dist-packages\/numpy\/core\/include/g' Makefile.config
+if [[ $(nvidia-smi) ]]; then
+    echo "FOUND NVIDIA-SMI, will try to compile Caffe with GPU support !"
+    # CUDA must be accessible, so define the environment variables below to suit your local setup. The same variables should be
+    # available when actually running VIC.
+    export PATH=/usr/local/cuda/bin:$PATH
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/lib
+else
+    echo "DID NOT FOUND NVIDIA-SMI, will compile Caffe for CPU only !"
+    sed -i 's/# CPU_ONLY/CPU_ONLY/g' Makefile.config
+fi
 sed -i 's/INCLUDE_DIRS :=/INCLUDE_DIRS := \/usr\/include\/hdf5\/serial\/ /g' Makefile.config
 sed -i 's/LIBRARY_DIRS :=/LIBRARY_DIRS := \/usr\/lib\/x86_64-linux-gnu\/hdf5\/serial\/ /g' Makefile.config
 sed -i 's/# Configure build/CXXFLAGS += -std=c++11/g' Makefile
@@ -159,13 +163,13 @@ make
 make install
 
 # config vgg_classifier
-sed -i 's/<full_path_to_this_directory>/\/webapps\/visorgen\/vgg_classifier/g' /webapps/visorgen/vgg_classifier/config.prototxt
-sed -i 's/<full_path_to_base_folder_where_images_in_dsetpaths_sample.txt_are_located>/\/webapps\/visorgen\/datasets\/images\/mydataset/g' /webapps/visorgen/vgg_classifier/config.prototxt
-sed -i 's/<full_path_to_base_folder_where_images_in_negpaths_sample.txt_are_located>/\/webapps\/visorgen\/datasets\/negatives\/mydataset/g' /webapps/visorgen/vgg_classifier/config.prototxt
-sed -i 's/<full_path_to_dsetfeats_binaryproto_file_produced_by_cpuvisor_preproc>/\/webapps\/visorgen\/backend_data\/cpuvisor-srv\/dsetfeats.binaryproto/g' /webapps/visorgen/vgg_classifier/config.prototxt
-sed -i 's/<full_path_to_negpaths_binaryproto_file_produced_by_cpuvisor_preproc>/\/webapps\/visorgen\/backend_data\/cpuvisor-srv\/negfeats.binaryproto/g' /webapps/visorgen/vgg_classifier/config.prototxt
-sed -i 's/negpaths_sample/negpaths/g' /webapps/visorgen/vgg_classifier/config.prototxt
-sed -i 's/dsetpaths_sample/dsetpaths/g' /webapps/visorgen/vgg_classifier/config.prototxt
+sed -i 's|<full_path_to_this_directory>|/webapps/visorgen/vgg_classifier|g' /webapps/visorgen/vgg_classifier/config.prototxt
+sed -i 's|<full_path_to_base_folder_where_images_in_dsetpaths_sample.txt_are_located>|/webapps/visorgen/datasets/images/mydataset|g' /webapps/visorgen/vgg_classifier/config.prototxt
+sed -i 's|<full_path_to_base_folder_where_images_in_negpaths_sample.txt_are_located>|/webapps/visorgen/datasets/negatives/mydataset|g' /webapps/visorgen/vgg_classifier/config.prototxt
+sed -i 's|<full_path_to_dsetfeats_binaryproto_file_produced_by_cpuvisor_preproc>|/webapps/visorgen/backend_data/cpuvisor-srv/dsetfeats.binaryproto|g' /webapps/visorgen/vgg_classifier/config.prototxt
+sed -i 's|<full_path_to_negpaths_binaryproto_file_produced_by_cpuvisor_preproc>|/webapps/visorgen/backend_data/cpuvisor-srv/negfeats.binaryproto|g' /webapps/visorgen/vgg_classifier/config.prototxt
+sed -i 's|/webapps/visorgen/vgg_classifier/dsetpaths_sample.txt|/webapps/visorgen/backend_data/cpuvisor-srv/dsetpaths.txt|g' /webapps/visorgen/vgg_classifier/config.prototxt
+sed -i 's|/webapps/visorgen/vgg_classifier/negpaths_sample.txt|/webapps/visorgen/backend_data/cpuvisor-srv/negpaths.txt|g' /webapps/visorgen/vgg_classifier/config.prototxt
 
 # configure vgg_frontend
 echo '%45yak9wu56^(@un!b+&022fdr!-1@92_u*gctw*cw4*@hfu5t' > /webapps/visorgen/secret_key_visorgen
